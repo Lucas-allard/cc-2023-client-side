ff3b9a70875781589e4dddecc60819fcca4db355
Lucas Allard <allard.lucas.97@gmail.com>
2023-10-30 12:05:28 +0100

    #1 renommage component header -> Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
