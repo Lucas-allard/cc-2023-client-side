7b0f62bfaf3868b23b5666851fb5d16fa1fe0466
Lucas Allard <allard.lucas.97@gmail.com>
2023-10-30 16:38:15 +0100

    update next js config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,25 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHouse} from '@fortawesome/free-solid-svg-icons'
 import {faPlane} from '@fortawesome/free-solid-svg-icons'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header className="p-4 bg-amber-400 text-white">
-            <div className="flex flex-nowrap justify-between items-center gap-4">
-                <div className="flex flex-col items-center gap-2">
+            <ul className="flex flex-nowrap justify-between items-center gap-4 ">
+                <li className="flex flex-col items-center gap-2 cursor-pointer hover:text-amber-200">
                     <FontAwesomeIcon icon={faHouse}/>
-                    <p className="text-xl font-bold">Accueil</p>
-                </div>
-                <div className="flex flex-col items-center gap-2">
+                    <Link href="/" className="text-xl font-bold">Accueil</Link>
+                </li>
+                <li className="flex flex-col items-center gap-2 cursor-pointer hover:text-amber-200">
                     <FontAwesomeIcon icon={faPlane}/>
                     <p className="text-xl font-bold">Voyages</p>
-                </div>
-                <div className="flex flex-col items-center gap-2">
+                </li>
+                <li className="flex flex-col items-center gap-2 group cursor-pointer hover:text-amber-200">
                     <FontAwesomeIcon icon={faUser}/>
                     <p className="text-xl font-bold">Profil</p>
-                </div>
-            </div>
+                </li>
+            </ul>
         </header>
     )
 }
