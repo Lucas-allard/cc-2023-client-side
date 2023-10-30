b129416da805fabf7063dcce1a760ec4f769efe5
nacer <nacermarwane@gmail.com>
2023-10-30 14:41:03 +0100

    #5 Placement du button

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,10 +2,12 @@ import Button from "@/components/Button";
 
 const Searchbar = () => {
     return (
-        <Button
-            cssClass="bg-amber-400"
-            label="Rechercher"
-        />
+        <div className='fixed bottom-0 w-full bg-white p-2 cursor-pointer'>
+            <Button
+                cssClass="bg-amber-400 w-full text-3xl p-2 hover:bg-amber-300"
+                label="Rechercher"
+            />
+        </div>
     )
 }
 
